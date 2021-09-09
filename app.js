16aecc8a31432888feb729e9c144cf2f76cb9768
Abhinav Upadhyay <61277379+Abhi02-pro@users.noreply.github.com>
2021-09-09 16:23:29 +0530

    update skill section

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 // adding active class to the links
 
+const sections = document.querySelectorAll("div .section");
+const navItems = document.querySelectorAll("nav div a");
+
+window.addEventListener("scroll", ()=>{
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(window.pageYOffset >= (sectionTop - sectionHeight/3)){
+            current = section.getAttribute("id");            
+        }
+    })
+    
+    navItems.forEach(item =>{
+        item.classList.remove("active");
+        if(item.classList.contains(current)){
+            item.classList.add("active")
+        }
+    })
+})
 
 
 
@@ -7,33 +28,33 @@
 
 // Timeline
 
-const tl = gsap.timeline({defaults:{ease: "power1.out"}});
+// const tl = gsap.timeline({defaults:{ease: "power1.out"}});
 
-tl.to(".text", {y:"0%", duration: 1, stagger: 0.75});
+// tl.to(".text", {y:"0%", duration: 1, stagger: 0.75});
 
-tl.to(".intro", {y: "-100%", duration: 1, delay:1.2})
+// tl.to(".intro", {y: "-100%", duration: 1, delay:1.2})
 
-tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 3})
+// tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 3})
 
-tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration:3}, "-=3")
+// tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration:3}, "-=3")
 
-tl.fromTo(".arrow", 2 ,{y:-50, opacity:0}, {y:5, opacity:1, repeatDelay:0.75, repeat:-1, }, "-=2")
+// tl.fromTo(".arrow", 2 ,{y:-50, opacity:0}, {y:5, opacity:1, repeatDelay:0.75, repeat:-1, }, "-=2")
 
 
 
-// FullPage
+// // FullPage
 
-new fullpage('#fullpage', {
-  autoScrolling: true,
-  navigation: true,
-  onLeave(origin, destination, direction){
-      const section = destination.item;
-      const tline = new TimelineMax({delay: 0.75})
-      if(destination.index === 1){
-        const myself = document.querySelector(".myself");
-        const education = document.querySelector(".education");
-        tline.fromTo(myself, 2, {x: -100, opacity:0}, {x: 0, opacity:1})
-            .fromTo(education, 2, {x: 100, opacity:0}, {x: 30, opacity:1}, "-=2")
-      }
-  }
-})
+// new fullpage('#fullpage', {
+//   autoScrolling: true,
+//   navigation: true,
+//   onLeave(origin, destination, direction){
+//       const section = destination.item;
+//       const tline = new TimelineMax({delay: 0.75})
+//       if(destination.index === 1){
+//         const myself = document.querySelector(".myself");
+//         const education = document.querySelector(".education");
+//         tline.fromTo(myself, 2, {x: -100, opacity:0}, {x: 0, opacity:1})
+//             .fromTo(education, 2, {x: 100, opacity:0}, {x: 30, opacity:1}, "-=2")
+//       }
+//   }
+// })
