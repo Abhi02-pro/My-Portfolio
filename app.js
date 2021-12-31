8f4d1f81512e9364882f8b0e38bae9c77cc14925
Abhinav Upadhyay <61277379+Abhi02-pro@users.noreply.github.com>
2021-12-31 14:22:57 +0530

    update skills section and home, add internsip, remove comments from app.js

--- app.js
+++ app.js
@@ -27,31 +27,6 @@ window.addEventListener("scroll", ()=>{
         }
     })
 
-
-    // technical skills animation
-
-    const skillsTop = skillsSection.getBoundingClientRect().top;
-    const screenPos = window.innerHeight;
-    // if(skillsTop < screenPos/3.5 && (skillsTop*(-1)) < screenPos/2){
-    if(skillsTop < screenPos/3.5){
-        techSkills.forEach(skill => {
-            skill.classList.add("active-bar")
-        })
-        proSkills.forEach(skill => {
-            skill.classList.add("active-circle")
-        })
-    }
-    else{
-        techSkills.forEach(skill => {
-            skill.classList.remove("active-bar")
-        })
-        proSkills.forEach(skill => {
-            skill.classList.remove("active-circle")
-        })
-    }
-
-
-
 })
 
 
@@ -80,38 +55,3 @@ const navSlide = ()=>{
 navSlide();
 
 
-
-// Animations
-
-// Timeline
-
-// const tl = gsap.timeline({defaults:{ease: "power1.out"}});
-
-// tl.to(".text", {y:"0%", duration: 1, stagger: 0.75});
-
-// tl.to(".intro", {y: "-100%", duration: 1, delay:1.2})
-
-// tl.fromTo(".big-text", {opacity: 0}, {opacity: 1, duration: 3})
-
-// tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration:3}, "-=3")
-
-// tl.fromTo(".arrow", 2 ,{y:-50, opacity:0}, {y:5, opacity:1, repeatDelay:0.75, repeat:-1, }, "-=2")
-
-
-
-// // FullPage
-
-// new fullpage('#fullpage', {
-//   autoScrolling: true,
-//   navigation: true,
-//   onLeave(origin, destination, direction){
-//       const section = destination.item;
-//       const tline = new TimelineMax({delay: 0.75})
-//       if(destination.index === 1){
-//         const myself = document.querySelector(".myself");
-//         const education = document.querySelector(".education");
-//         tline.fromTo(myself, 2, {x: -100, opacity:0}, {x: 0, opacity:1})
-//             .fromTo(education, 2, {x: 100, opacity:0}, {x: 30, opacity:1}, "-=2")
-//       }
-//   }
-// })
